b85e47d94366449fc3794c880b11496063c9355d
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2022-11-09 16:22:59 +0100

    Reflection v.1

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,6 +1,9 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberSwingView;
@@ -24,10 +27,23 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws Exception{
+        
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberViewImpl());
+        /*app.addView(new DrawNumberViewImpl());
         app.addView(new DrawNumberSwingView());
+        */
+        final Class<?> view1 = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+        final Class<?> view2 = Class.forName("it.unibo.mvc.view.DrawNumberViewImpl");
+        final Constructor<?> c1 = view1.getConstructor();
+        final Constructor<?> c2 = view2.getConstructor();
+        final Object v1 = c1.newInstance();
+        final Object v2 = c2.newInstance();
+        for(int i = 0; i < 3; i++){
+            app.addView((DrawNumberView)c1.newInstance());
+            app.addView((DrawNumberView)c2.newInstance());
+        }
+
     }
 }
